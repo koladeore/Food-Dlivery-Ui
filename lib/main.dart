e9e399ad10eb9cefbdd3f2d524605c31b607547c
koladeore <koladeore@gmail.com>
2021-01-02 12:53:06 +0100

    first-commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery/screens/home_screens.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Food Delivery',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.grey[50],
+        primaryColor: Colors.deepOrangeAccent,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/models/order.dart
@@ -0,0 +1,14 @@
+import 'food.dart';
+import 'restaurant.dart';
+class Order{
+  final Restaurant restaurant;
+  final Food food;
+  final String date;
+  final int quantity;
+  Order({
+    this.restaurant,
+    this.food,
+    this.date,
+    this.quantity
+  });
+}
